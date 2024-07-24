544a59e321a625713e7ae9c81b5a1f4d4c721804
~nz <81528500+ProtoSparky@users.noreply.github.com>
2024-07-24 16:12:10 +0200

    pushy :3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,205 +1,49 @@
 var Settings = {
-    "SideBar":{
-        "FolderSize":{
-            "x": 300,
-            "y": 50,
-            "offset":10
-        },
-        "NestedFolderSize":{
-            "y":4,
-            "offset":35,
-            "VisualHight":40
-        },
-        "size":{
-            "w":300
-        },
-        "current_nested_object":0,
-        "prev_lnk":null
-        
+    "Explorer":{
+        "style":{
+            "width":300
+        }
+    },
+    "Directory":"./assets/file_index/index.json",
+    "SelectedHome":null,
+    "Home":{
+        "style":{
+            "gap":50
+        }
     }
-}
-var FileIndex = undefined;
+};
+var DirectoryData = null; 
 function init(){
-    //we try to get the JSON object
-    const JsonFileLoc = "./assets/file_index/index.json";
-    FileIndex = ReadJSON(JsonFileLoc, false);
-    
-    console.info("Everything loaded. Spawning UI");
-    //everything Ok, launch rest of code
-    const Root = document.getElementById("content-fullscreen");
-    
-    //we spawn the sidebar
-    const SideBar = document.createElement("div");
-    SideBar.id = "SideBar"; 
-    SideBar.style.left = "0px";
-    SideBar.style.top = "0px";
-    SideBar.style.position = "absolute"; 
-    SideBar.style.height = "100%";
-    SideBar.style.width = Settings.SideBar.size.w;
-    SideBar.style.backgroundColor = AccessCSSVar("--col_bg_lighter");
-    Root.appendChild(SideBar);
-    document.body.style.backgroundColor = AccessCSSVar("--col_bg_content");
+    console.info("starting CE");
+
+    //spawn sidebar
+    const Explorer = document.createElement("div");
+    Explorer.style.top = "0px";
+    Explorer.style.position = "absolute";
+    Explorer.style.left = "0px";
+    Explorer.style.height = "100%";
+    Explorer.style.width = Settings.Explorer.style.width;
+    Explorer.style.backgroundColor = AccessCSSVar("--col_bg_lighter"); 
+    const Body = document.getElementById("content-fullscreen"); 
+    Body.appendChild(Explorer); 
 
 
-    const Viewer = document.createElement("iframe");
-    Viewer.id = "Viewer";
-    Viewer.style.left = Settings.SideBar.size.w;
-    Viewer.style.position = "absolute";
-    Viewer.style.top = 0;
-    Viewer.style.width = window.innerWidth - Settings.SideBar.size.w - 30;
-    Viewer.style.height = window.outerHeight;
-    Root.appendChild(Viewer);
+    //we load the Directory listing
+    DirectoryData = ReadJSON(Settings.Directory, false);
 
-    ShowRoot(FileIndex);    
-}
-
-function SpreadFolder(FolderName){
-    const SideBar = document.getElementById("SideBar");
-    SideBar.innerHTML = "";
-    //spawn tree
-    Tree(FolderName);
+    //we load the home screen
+    Home(); 
 
 }
 
-function Back(){
-    const SideBar = document.getElementById("SideBar");
-    SideBar.innerHTML= "";
-    ShowRoot(FileIndex);
-}
-
-function Tree(FolderName){
-    //we iterate trough all folders
-    const current_selected_folder = FileIndex[FolderName];
-    Settings.SideBar.prev_lnk  = FolderName; 
-
-    //visualizeJSON(current_selected_folder,"SideBar");
-    //extend the ui for the display size
-    traverseObjectDepthFirst(current_selected_folder)
-}
-
-function traverseObjectDepthFirst(obj, depth = 0, path = []) {
-    for (let key in obj) {
-        let value = obj[key];
-        let type = typeof value;
-
-        // Check if the current item is a key or a value
-        let isKey = type === "object" &&!Array.isArray(value);
-        const container = document.getElementById("SideBar");
-        const current_key_name = key;
-        const val = document.createElement("div");
-        val.style.position = "absolute";
-        val.style.left = depth * 10;        
-        val.className = "text";
-        val.style.color = "white";
-        val.style.width = "100%";
-        val.style.height = Settings.SideBar.FolderSize.y;
-        let newPath = path.concat(key);
-
-        if(isKey){
-            //it is a key     
-            val.style.top = Settings.SideBar.current_nested_object * Settings.SideBar.NestedFolderSize.y;
-            //val.innerHTML = current_key_name;
-            const img = document.createElement("img");
-            img.src = "./assets/img/folder.svg";
-            img.style.position = "absolute";
-            img.style.top = "50%";
-            img.style.left = 0;
-            img.style.transform = "translate(0,-50%)";
-            img.style.height = "100%";
-            img.style.width = "30px";
-
-            const text = document.createElement("div");
-            text.style.position = "absolute";
-            text.style.left = "20px";
-            text.style.top = "50%";
-            text.style.transform = "translate(0,-50%)";
-            text.className = "text";
-            text.innerHTML = current_key_name; 
-
-            val.appendChild(img);
-            val.appendChild(text);                        
-        }
-        else{
-            //is value
-            //val.innerHTML = value;
-            for(let pointer = 0; pointer < value.length; pointer ++){
-                const current_val = value[pointer];
-                const val2 = document.createElement("div");
-                val2.style.position  ="absolute";
-                val2.style.left = depth * 50;
-                val2.style.color = "white";
-                val2.style.width = "100%";
-                val2.className = "text";
-                val2.innerHTML = truncateString(current_val, 30);
-                val2.style.top = (Settings.SideBar.current_nested_object * Settings.SideBar.NestedFolderSize.y) + Settings.SideBar.NestedFolderSize.offset;
-                val2.style.height = Settings.SideBar.NestedFolderSize.VisualHight; 
-                Settings.SideBar.current_nested_object = Settings.SideBar.current_nested_object + 10;
-
-                //create a valid link
-                link = "./exports/"+Settings.SideBar.prev_lnk + "/"; 
-                for(current_link_pointer = 0; current_link_pointer < path.length; current_link_pointer ++){
-                    link = link + path[current_link_pointer] + "/";
-                }
-
-                val2.addEventListener("click",function(){
-                    SpawnIframe(link + current_val);
-                })
-                val.style.cursor = "pointer";
-                val.appendChild(val2);
-            }
-        }
-        container.appendChild(val);
-        //iterate the current nested object size 
-        Settings.SideBar.current_nested_object ++; 
-        if (isKey) {
-            traverseObjectDepthFirst(value, depth + 1, newPath);
-        }
-    }
-}
-
-
-function SpawnIframe(Link){
-    console.log(Link);
-    const Viewer = document.getElementById("Viewer");
-    Viewer.style.backgroundColor = "white";
-    Viewer.src = Link;
-}
-
-function ShowRoot(FileIndex){
-    //we crawl the FileIndex and create the root folders
-    const SideBar = document.getElementById("SideBar");
-    const TopDirKeys = Object.keys(FileIndex);
-    const TopDirKeylength = TopDirKeys.length;
-    for(let TopPointer = 0; TopPointer < TopDirKeylength; TopPointer ++){
-        const current_key_name = TopDirKeys[TopPointer];
-        const current_key_object = FileIndex[current_key_name];
-
-
-        const CurrentFolder = document.createElement("div");
-        CurrentFolder.style.position = "absolute";
-        CurrentFolder.style.width = Settings.SideBar.FolderSize.x;
-        CurrentFolder.style.height = Settings.SideBar.FolderSize.y;
-        CurrentFolder.style.top = Settings.SideBar.FolderSize.y * TopPointer;
-        CurrentFolder.style.left = 0;
-        CurrentFolder.id = current_key_name; 
-        CurrentFolder.style.cursor = "pointer";
-        CurrentFolder.addEventListener("click", function(){
-            SpreadFolder(current_key_name);
-        });
-        SideBar.appendChild(CurrentFolder);
-        const CurrentFolderHeader = document.createElement("div");
-        CurrentFolderHeader.style.position = "absolute"; 
-        CurrentFolderHeader.style.left = 8;
-        CurrentFolderHeader.style.top = "50%";
-        CurrentFolderHeader.innerHTML = current_key_name;
-        CurrentFolderHeader.class = "text";
-        CurrentFolderHeader.style.transform = "translate(0,-50%)";
-        CurrentFolderHeader.style.color = AccessCSSVar("--col_normalTXT");
-        CurrentFolder.appendChild(CurrentFolderHeader);      
-
+function Home(){
+    //this function will display the choise data for the home directories (I know this is confusing)
+    const home_keys = Object.keys(DirectoryData);
+    const home_key_amounts = home_keys.length;
+    for(let home_key_pointer = 0; home_key_pointer < home_key_amounts; home_key_pointer ++){
+        const current_key = home_keys[home_key_pointer];
+        const directory = document.createElement("div");
         
-    }
-}
-function ApplyStyle(){
 
+    }
 }
--- cheeks.py
+++ cheeks.py
@@ -4,6 +4,7 @@ import http.server
 import socketserver
 import webbrowser
 
+'''
 def find_htm_files(start_dir):
     htm_map = {}
     for root, dirs, files in os.walk(start_dir):
@@ -19,9 +20,38 @@ def find_htm_files(start_dir):
             current_level[last_part] = htm_files
     return htm_map
 
+'''
 def save_to_json(htm_map, filename='./assets/file_index/index.json'):
     with open(filename, 'w') as f:
         json.dump(htm_map, f, indent=4)
+
+def find_htm_files(start_dir):
+    result = {}
+
+    for root, dirs, files in os.walk(start_dir):
+        htm_files = [file for file in files if file.endswith('.htm') or file.endswith('.html')]
+        if htm_files:
+            # Get the relative path and split it into directory structure
+            relative_path = os.path.relpath(root, start_dir)
+            dir_structure = relative_path.split(os.sep) if relative_path != '.' else []
+            
+            # Use the first part of the directory structure as the base key
+            base_dir_name = dir_structure[0] if dir_structure else os.path.basename(start_dir)
+            
+            # Initialize the base directory in the result if it doesn't exist
+            if base_dir_name not in result:
+                result[base_dir_name] = {}
+
+            # Create an entry for this specific directory
+            index = len(result[base_dir_name])  # Use the current count of entries as index
+            I_dir = [f"{dir}{i}" for i, dir in enumerate(dir_structure)]
+            result[base_dir_name][str(index)] = {
+                "dir": dir_structure,
+                "I_dir": I_dir,
+                "files": htm_files
+            }
+
+    return result
 
 
 
@@ -37,8 +67,10 @@ def launch_html_server(directory):
 
 
 if __name__ == "__main__":
-    start_directory = './exports'
+    ##DEV   start_directory = './exports'
+    start_directory = '../../../Downloads/OneNoteBackup'
     index_loc = './assets/file_index/index.json' 
+    
     try:
         os.remove(index_loc) #remove temp file if code crashed previous run
     except:
