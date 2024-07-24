74f4a7d28572c0e4b727c4c9a3f638576795ab66
~nz <81528500+ProtoSparky@users.noreply.github.com>
2024-07-24 18:08:29 +0200

    updating ui more

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,19 +12,21 @@ var Settings = {
         }
     },
     "OpenDir":{
-        "current_directory":{
+        "dir_display":{
             "style":{
-
+                "box_height":150,
+                "header_top":0,
+                "header_height":20
             }
         },
-        "dir_display":{
+        "dir_header":{
             "style":{
                 "height":50
             },
             "header":"./"
         }
     },
-    "temp_data":{}
+    "temp_data":{}//the data stored here will change and be mutable
 };
 var DirectoryData = null; 
 function init(){
@@ -42,7 +44,7 @@ function init(){
     const Body = document.getElementById("content-fullscreen"); 
     Body.appendChild(Explorer); 
 
-
+    document.body.style.backgroundColor = AccessCSSVar("--col_bg_content");
     //we load the Directory listing
     DirectoryData = ReadJSON(Settings.Directory, false);
 
@@ -89,7 +91,7 @@ function OpenDir(dirname){
     for(let dir_pointer = 0; dir_pointer < total_dir_length; dir_pointer ++){
         const select_dir = SelectData[dir_pointer];
         const file_length = select_dir.files.length;
-        const select_2nd_key = select_dir.dir[1];
+        const select_2nd_key = select_dir.I_dir[1];
         if(length_counter[select_2nd_key] == undefined){
             length_counter[select_2nd_key] = 0;
         }
@@ -104,19 +106,71 @@ function OpenDir(dirname){
     dir_name.style.top = 0;
     dir_name.style.left = 0;
     dir_name.style.width = "100%";
-    dir_name.style.height = Settings.OpenDir.dir_display.style.height; 
+    dir_name.style.height = Settings.OpenDir.dir_header.style.height; 
     dir_name.id = "DirName";
     dir_name.className = "text";
     dir_name.style.color = AccessCSSVar("--col_bold_TXT");
     dir_name.style.fontWeight = 800;
     dir_name.style.textAlign = "center";
     dir_name.style.cursor = "pointer";
-    dir_name.innerHTML = Settings.OpenDir.dir_display.header + dirname + "/";
+    dir_name.innerHTML = Settings.OpenDir.dir_header.header + dirname + "/";
     dir_name.addEventListener("click", function(){
         Home();
     });
     body.appendChild(dir_name);
 
 
+    //we add the frame all directories will stay in
+    const dir_frame = document.createElement("div");
+    dir_frame.id = "dir_frame";
+    dir_frame.style.position = "absolute";
+    dir_frame.style.top = Settings.OpenDir.dir_header.style.height;
+    dir_frame.style.height = "100%";
+    dir_frame.style.width = "100%";
+    body.appendChild(dir_frame); 
+
+
+
+    //we load in all base directories
+    const base_directory_array = Object.keys(length_counter);
+    const base_directory_count = base_directory_array.length; 
+    Settings.temp_data.base_directory_pointer = 0; //clear pointer before it is used
+    for(let base_directory_pointer = 0; base_directory_pointer < base_directory_count; base_directory_pointer++){
+        const current_base_dir_name = base_directory_array[base_directory_pointer]; 
+        const current_base_dir = length_counter[current_base_dir_name];
+        const base_directory_select = document.createElement("div")
+        base_directory_select.style.position  ="absolute";
+        base_directory_select.style.width = "100%";
+        base_directory_select.style.left = 0;
+        base_directory_select.id = current_base_dir_name;
+        base_directory_select.style.height = Settings.OpenDir.dir_display.style.box_height * current_base_dir;
+
+        base_directory_select.style.top = Settings.temp_data.base_directory_pointer; 
+        Settings.temp_data.base_directory_pointer = Settings.temp_data.base_directory_pointer + Settings.OpenDir.dir_display.style.box_height * current_base_dir;
+        dir_frame.appendChild(base_directory_select); 
+
+        //add base directory headers
+        const header = document.createElement("div");
+        header.innerHTML = current_base_dir_name;
+        header.style.position = "absolute";
+        header.style.left = 0;
+        header.style.top = Settings.OpenDir.dir_display.style.header_top;
+        header.style.height = Settings.OpenDir.dir_display.style.header_height;
+        header.style.width = "100%";
+        header.style.color = AccessCSSVar("--col_bold_TXT");
+        header.style.fontWeight = 900;
+        header.style.textAlign = "center";
+        header.style.className = "text";
+        base_directory_select.appendChild(header);
+
+        //clear temp directory top pointers
+        Settings.temp_data[current_base_dir_name] = 0;
+    }
+
+    //increase size of sidebar
+    body.style.height = Settings.temp_data.base_directory_pointer;
+
+    //iterate over all files and append them to their respected places
+    
 
 }
